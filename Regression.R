4e335534c51b7fd98253db922b98c8d58114594c
Arifa Abbas <arifaabbas@Arifas-MacBook-Pro.local>
2021-12-13 12:57:08 -0500

    Updated regression

--- Regression.R
+++ Regression.R
@@ -1,4 +1,4 @@
-#package needed
+#package needed -
 #library(crayon)
 
 
@@ -9,77 +9,65 @@ mreg <- function(formula, data){
   class(fit) <- c("mreg", "lm")
   return(fit)
 }
-
-#things to consider -
-#1. only write and explain about significant p values but unsure how to extract those
-#2. how to have user inputting more than 3 variables and perhaps create a for loop that that will
-#   input all those variables into the function as required
-#3. how to get the explanation to also loop through and explain all significant p values no matter how
-#   many there are
-#4. How to change name of varibale from data[[a]] to the actual name
-#5. Include any r squared explanations
-#6. If the values are logged and if I should offer an option for logged values in which case
-#   the results would be described differently
 data <- mtcars
 library(crayon)
-# working function 1 ----
-super.mreg <- function(a, b, c){
-  fit <- mreg(data[[a]] ~ data[[b]] + data[[c]], data)
-  print(summary(fit))
-  coef <- fit$coefficients
-  cat("Do you need help understanding your results?
-  1.Yes
-  2.No")
-  select <- readline("Please enter the number associated with the option you want:")
-  if (select == 1){
-    cat("Each time", red(a), "went up by a factor of 1,", red(b), "changes by a factor of", bold(red(coef[2])),"\n", seq = "")
-    cat("Each time", red(a), "went up by a factor of 1,", red(c), "changes by a factor of", bold(red(coef[3])), seq = "")
-
-  }
-}
-
 
 #spaces between variables when they are wrong for independent variables
-# NEWEST VERSION ----
+# NEWEST VERSION that works very well----
 data <- mtcars
 
 super.mreg <- function(){
   cat("Welcome to conducting a multiple regression!", "\n",
-      "Please enter the exact name of the dependent variable (i.e predicted variable)", sep = "")
-  input1 <- readline("Enter here and hit return:")
-  if (length(setdiff(input1, names(data))) == 0){
-    cat("Thanks!","\n")
-    print(input1)
-  }else{
-    cat("'", bold(setdiff(input1, names(data))),"'", " is an", bold(red(" invalid"))," variable that is not in the dataset", "\n",
-        "Now we will go back to the begining, please ensure you enter a valid variable", "\n",
-        "that is in the dataset","\n", sep = "")
-    input1 = character(0)
-    super.mreg()
+      "Would you like a brief overview of what a multiple regression entails?
+  1.Yes
+  2.No")
+  overview <- readline("Please enter the number associated with the option you want:")
+  if (overview == 1){
+    cat("Multiple linear regression, also known simply as multiple regression, is a statistical technique 
+that uses several explanatory variables (i.e predictor variables or independent variables)
+to predict the outcome of a response variable (i.e predicted variable or dependent variable). 
+Multiple regression is an extension of simple linear regression because in a multiple regression
+you can input more than one explanatory variable.","\n", sep = "")
+  }
+  cat("Now will be asked to input the variable you want to predict.", "\n",
+      "Please ensure you enter the exact name of the dependent variable (i.e predicted variable)", sep = "")
+  input1 = 0
+  while(input1 == 0){
+    dv <- readline("Enter dependent variable name here and hit return:")
+    if(length(setdiff(dv, names(data))) > 0){
+      cat("'", bold(setdiff(dv, names(data))),"'", " is an", bold(red(" invalid"))," variable that is not in the dataset.", "\n",
+          "Please input a variable that is in the dataset.", sep = "")
+    }else{
+      input1 <- dv
+      print(input1)
+      cat("Thanks!", "\n")
+    }
   }
   cat("Now you will be asked to enter the exact names of the independent variables
-(i.e predictor variables)")
-  input2 <- readline("Enter the independent variables seperated by a comma:")
-  input2 <- strsplit(input2, ",")
-  input2 <- input2[[1]]
-  if (length(setdiff(input2, names(data))) > 0){
-    cat("The following variables are ", bold(red("invalid ")),"as they are not in the dataset:","\n",
-        bold(setdiff(input2, names(data))),"\n", sep = "")
-    cat("Now we will go back to the begining, please ensure you enter a valid variables", "\n",
-        "that are in the dataset","\n", sep = "")
-    input2 = character(0)
-    super.mreg()
-  }else{
-    cat("Thanks!","\n")
+(i.e predictor variables) that will test the relationship with the depedent variable")
+  input2 = 0
+  while(input2 == 0){
+    iv <- readline("Please enter the independent variables seperated by a space:")
+    iv <- strsplit(iv, " ")
+    iv <- iv[[1]]
+    if (length(setdiff(iv, names(data))) > 0){
+      cat("The following variables are ", bold(red("invalid ")),"as they are not in the dataset:","\n",
+          bold(setdiff(iv, names(data))),"\n", 
+          "Please input a variable that is in the dataset.", sep = "")
+    }else{
+      input2 <- iv
+      print(input2)
+      cat("Thanks!","\n")
+    }
   }
-  cat("Here are the results for the multiple regression:")
+  cat(bold(green("Here are the results for the multiple regression:")))
   print("test")
   ls <- input1
   rs <- paste(input2, collapse="+")
   f <- paste(ls, "~", rs)
   formula <- as.formula(f)
   fit <- mreg(formula, data)
-  print(summary(fit))
+  print(summary.lm(fit))
   results <- summary.lm(fit)
   results <- summary.lm(fit)$coefficients
   cat("Do you need help understanding your results?
@@ -87,32 +75,85 @@ super.mreg <- function(){
   2.No")
   select <- readline("Please enter the number associated with the option you want:")
   if (select == 1){
+    cat(underline("Here are the significant results where the p value was less than 0.05:"),"\n", sep = "")
     for(i in 2:nrow(results)){
       if(results[i, 4] < .05){
-        cat("Here are your significant results:")
-        cat("For a 1 pt change in",  dimnames(results)[[1]][i],
-            ",", input1, "is expected to change by", results[i, 1], "\n")
-        cat("This will be the r-squared value")
-        }
+        cat("For a 1 point change in",  bold(dimnames(results)[[1]][i]),
+            ",", blue(input1), "is expected to change by", red(results[i, 1]),".", "\n", sep = "")
+      }
     }
+    cat("Do you want to further understand the multiple regression by looking at the r-sqaured value? 
+        1.Yes
+        2.No")
+    rsq
+    cat("This will be the r-squared value")
+    
+  }
+}
 
-}}
-
-
-library(crayon)
 super.mreg()
-hinput1
-data <- mtcars
-input1
-input2 <- c("hp", "wt", "am")
+
+
+
+# super.mreg <- function(){
+#   cat("Welcome to conducting a multiple regression!", "\n",
+#       "Please enter the exact name of the dependent variable (i.e predicted variable)", sep = "")
+#   input1 <- readline("Enter here and hit return:")
+#   if (length(setdiff(input1, names(data))) == 0){
+#     cat("Thanks!","\n")
+#     print(input1)
+#   }else{
+#     cat("'", bold(setdiff(input1, names(data))),"'", " is an", bold(red(" invalid"))," variable that is not in the dataset", "\n",
+#         "Now we will go back to the begining, please ensure you enter a valid variable", "\n",
+#         "that is in the dataset","\n", sep = "")
+#     input1 = character(0)
+#     super.mreg()
+#   }
+#   cat("Now you will be asked to enter the exact names of the independent variables
+# (i.e predictor variables)")
+#   input2 <- readline("Enter the independent variables seperated by a comma:")
+#   input2 <- strsplit(input2, ",")
+#   input2 <- input2[[1]]
+#   if (length(setdiff(input2, names(data))) > 0){
+#     cat("The following variables are ", bold(red("invalid ")),"as they are not in the dataset:","\n",
+#         bold(setdiff(input2, names(data))),"\n", sep = "")
+#     cat("Now we will go back to the begining, please ensure you enter a valid variables", "\n",
+#         "that are in the dataset","\n", sep = "")
+#     input2 = character(0)
+#     super.mreg()
+#   }else{
+#     cat("Thanks!","\n")
+#   }
+#   cat("Here are the results for the multiple regression:")
+#   print("test")
+#   ls <- input1
+#   rs <- paste(input2, collapse="+")
+#   f <- paste(ls, "~", rs)
+#   formula <- as.formula(f)
+#   fit <- mreg(formula, data)
+#   print(summary(fit))
+#   results <- summary.lm(fit)
+#   results <- summary.lm(fit)$coefficients
+#   cat("Do you need help understanding your results?
+#   1.Yes
+#   2.No")
+#   select <- readline("Please enter the number associated with the option you want:")
+#   if (select == 1){
+#     for(i in 2:nrow(results)){
+#       if(results[i, 4] < .05){
+#         cat("Here are your significant results:")
+#         cat("For a 1 pt change in",  dimnames(results)[[1]][i],
+#             ",", input1, "is expected to change by", results[i, 1], "\n")
+#         cat("This will be the r-squared value")
+#         }
+#     }
+# 
+# }}
 
 
 
 
-
-
-
-hi# previous functions that may help ----
+# previous functions that may help ----
 # ttest <- function(data, x, y, digits = 2, col = "skyblue", ...){
 #   t_results <- t.test(data[[y]] ~ data[[x]])
 #   std <- aggregate(data[[y]], by=list(data[[x]]), FUN=sd)
